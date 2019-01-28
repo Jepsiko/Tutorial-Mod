684a2c9d59c7f0507f36375685d847a9be345c7f
Jepsiko <jepsiko@Gmail.com>
2019-01-28 22:53:35 +0100

    Added all the pieces of armor in ruby and their recipes according to this tutorial: https://www.youtube.com/watch?v=X0UfJlpdNFs

--- src/main/java/com/jepsiko/tutorialmod/init/ModItems.java
+++ src/main/java/com/jepsiko/tutorialmod/init/ModItems.java
@@ -1,7 +1,11 @@
 package com.jepsiko.tutorialmod.init;
 
 import com.jepsiko.tutorialmod.items.ItemBase;
+import com.jepsiko.tutorialmod.items.armor.ArmorBase;
 import com.jepsiko.tutorialmod.items.tools.*;
+import com.jepsiko.tutorialmod.util.Reference;
+import net.minecraft.init.SoundEvents;
+import net.minecraft.inventory.EntityEquipmentSlot;
 import net.minecraft.item.*;
 import net.minecraftforge.common.util.EnumHelper;
 
@@ -14,6 +18,8 @@ public class ModItems {
 
     //Materials
     public static final Item.ToolMaterial MATERIAL_RUBY = EnumHelper.addToolMaterial("material_ruby", 3, 250, 8.0f, 3.0f, 10);
+    public static final ItemArmor.ArmorMaterial ARMOR_MATERIAL_RUBY = EnumHelper.addArmorMaterial("armor_material_ruby", Reference.MOD_ID + ":ruby", 14,
+            new int[]{2, 5, 7, 3}, 10, SoundEvents.ITEM_ARMOR_EQUIP_DIAMOND, 0.0f);
 
     //Items
     public static final Item RUBY = new ItemBase("ruby");
@@ -25,4 +31,10 @@ public class ModItems {
     public static final ItemAxe RUBY_AXE = new ToolAxe("ruby_axe", MATERIAL_RUBY);
     public static final ItemHoe RUBY_HOE = new ToolHoe("ruby_hoe", MATERIAL_RUBY);
     public static final ItemSword RUBY_SWORD = new ToolSword("ruby_sword", MATERIAL_RUBY);
+
+    //Armor
+    public static final Item RUBY_HELMET = new ArmorBase("ruby_helmet", ARMOR_MATERIAL_RUBY, 1, EntityEquipmentSlot.HEAD);
+    public static final Item RUBY_CHESTPLATE = new ArmorBase("ruby_chestplate", ARMOR_MATERIAL_RUBY, 1, EntityEquipmentSlot.CHEST);
+    public static final Item RUBY_LEGGINGS = new ArmorBase("ruby_leggings", ARMOR_MATERIAL_RUBY, 2, EntityEquipmentSlot.LEGS);
+    public static final Item RUBY_BOOTS = new ArmorBase("ruby_boots", ARMOR_MATERIAL_RUBY, 1, EntityEquipmentSlot.FEET);
 }
